694999f81cde5d743f36f7b297023ed4d459f9ec
Hanlun Wang <wanghanlunjason@gmail.com>
2024-02-08 11:16:07 -0500

    add in class assignment

---
+++ server.js
@@ -0,0 +1,18 @@
+const express = require("express"); // "require" the Express module
+const app = express(); // obtain the "app" object
+const HTTP_PORT = process.env.PORT || 8080; // assign a port
+
+// start the server on the port and output a confirmation to the console
+app.listen(HTTP_PORT, () => console.log(`server listening on: ${HTTP_PORT}`));
+
+const path = require("path");
+
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "/views/home.html"));
+});
+
+app.get("/about", (req, res) => {
+  res.sendFile(path.join(__dirname, "/views/about.html"));
+});
+
+app.use(express.static('public'));
